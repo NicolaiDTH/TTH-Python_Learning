c91e0e7d909d111268724abca7fd00aef00a15d9
Nicolai <nicolai@mmg.io>
2014-11-13 16:13:07 +0000

    Another Python Lesson

---
+++ Hello.py
@@ -0,0 +1,5 @@
+age = 23
+days = age*52*7
+decades = age/10
+estimate = round(decades)
+summary = "I am {} days old! That's about {} decades!".format(days, str(estimate))
---
+++ shopping_list.py
@@ -0,0 +1,4 @@
+full_name = "Julie Walker"
+name_list = full_name.split()
+greeting_list = str.split("Hi, I'm {}".format(name_list[0]))
+greeting = ' '.join(greeting_list)
